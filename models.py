d6ac962109396c106b484118abeef098d194232d
DianaStreinu <dookie_ado@yahoo.com>
2025-02-23 15:38:55 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,16 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
 
 """SQLAlchemy is a Python library that allows us to work with a database using Python 
-classes instead of raw SQL queries."""
-db = SQLAlchemy()
+classes instead of raw SQL queries. The SQLAlchemy instance helps create and manage database tables."""
 
+db = SQLAlchemy() #This initializes the database
+
+# This defines a database model for inventory items.
+# The class represents a single table in the database.
 class InventoryItem(db.Model):
-    __tablename__ = 'inventory'
-
-    id = db.Column(db.Integer, primary_key=True)
-    item_name = db.Column(db.String(100), nullable=False)
+    __tablename__ = 'inventory' #This sets the table's name to "inventory" to ensure consistency.
+#Here, we define each of the table columns
+    id = db.Column(db.Integer, primary_key=True) #this defines an integer column in the database
+    item_name = db.Column(db.String(100), nullable=False) #This defines a string column with a maximum of 100 characters.
     type = db.Column(db.String(50), nullable=False)  # Ok, Damaged, Sent for maintenance
     status = db.Column(db.String(50), nullable=False)  # Free, In use
 
-    def __repr__(self):
+#This helps with representing the Object as a String
+#It helps debugging by making it easy to inspect objects.
+    def __repr__(self): #This is a method that controls how objects appear when printed.
         return f'<Item {self.id}: {self.item_name}, {self.type}, {self.status}>'
